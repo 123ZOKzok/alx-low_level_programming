432020b4d751490ceae2245bb9a689ee2dd33e56
Zipporah Ondiba <48816919+123ZOKzok@users.noreply.github.com>
2022-04-27 12:41:48 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,87 @@
+
+
+D
+D
+C
+C
+C
+
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+
+C
+
+#include "main.h"
+
+
+/**
+<<<<<<< HEAD
+* main - Entry point
+*
+* Return: nothing, return void
+*/
+=======
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+>>>>>>> 29abc3f21d41324eea0a3b96abb3ecfa8e3b3157
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+<<<<<<< HEAD
+return (0);
+}
+
+=======
+>>>>>>> 29abc3f21d41324eea0a3b96abb3ecfa8e3b3157
